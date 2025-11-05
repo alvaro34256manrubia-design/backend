6c1e9024ec9f298a928c9412b4341de9ca6a33a6
alcalman <alvaro.34256manrubia@gmail.com>
2025-11-05 19:41:34 +0100

    task 9 completado

--- Productos/views.py
+++ Productos/views.py
@@ -4,8 +4,25 @@ from .models import Productos
 
 
 
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Productos
+from .forms import UploadProductos
+
 @login_required(login_url="/users/login/")
 def product_list_view(request):
-    products = Productos.objects.filter(user=request.user)
-    return render(request, 'product_list.html', {'productos': 'products'})
-
+    if request.method == 'POST':
+        form = UploadProductos(request.POST)
+        if form.is_valid():
+            new_product = form.save(commit=False)
+            new_product.user = request.user
+            new_product.save()
+            return redirect('productos:lista')
+    else:
+        form = UploadProductos()
+    
+    products = Productos.objects.filter(user=request.user)[:20]
+    return render(request, 'list_product.html', {
+        'form': form,
+        'productos': products
+    })
